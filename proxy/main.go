19550ec6e214797994bec2c843bff1b1f0b069fc
David Thomas Nilsson <davidat@gmail.com>
2026-01-25 17:40:10 +0100

    Restore YRPROXY_API_KEY env var name

--- proxy/main.go
+++ proxy/main.go
@@ -142,8 +142,8 @@ const apiKeyHeader = "X-Api-Key"
 
 func main() {
 	// Require a configured API key since David doesn't want any public endpoints.
-	if os.Getenv("API_KEY") == "" {
-		log.Fatal("API_KEY is required")
+	if os.Getenv("YRPROXY_API_KEY") == "" {
+		log.Fatal("YRPROXY_API_KEY is required")
 	}
 
 	http.HandleFunc("/", proxyHandler)
@@ -160,7 +160,7 @@ func main() {
 }
 
 func requireApiKey(r *http.Request) bool {
-	expected := os.Getenv("API_KEY")
+	expected := os.Getenv("YRPROXY_API_KEY")
 	provided := r.Header.Get(apiKeyHeader)
 	if expected == "" || provided == "" {
 		return false
